a4c56f48e1cc9854fdaaf0034c2e7343fd6aac66
Sam Reyes <ksamuelreyes@gmail.com>
2023-01-24 15:55:16 -0700

    Commit

--- Mission_3/Program.cs
+++ Mission_3/Program.cs
@@ -5,16 +5,92 @@ namespace Mission_3
     class Program
     {
         static void Main (string[] args)
-        { 
-            Console.WriteLine("Hello World!");
-            char[] gameArray = new char[9] { 'O', 'O', 'O', 'X', 'O', 'O','O', 'X', 'O' };
- 
+        {
             Accept_Board ab = new Accept_Board();
-            ab.Board(gameArray);
+            string winner = ""; 
+            string[] boardArray = new string[9];
+            bool gameContinue = true;
+            bool inputContinue = true;
+            bool moveContinue = true;
+            int playerTurn = 1;
+            int movePosition = 10;
 
-            string winner;
-            winner = ab.winCheck(gameArray); 
-            Console.WriteLine(winner);
+            Console.WriteLine("Welcome to Tic Tac Toe");
+
+            while (gameContinue == true)
+            {
+                inputContinue = true;
+                playerTurn++;
+                if (playerTurn % 2 == 0)
+                {
+                    while (inputContinue == true)
+                    {   
+                        while (moveContinue == true)
+                        {
+                            Console.WriteLine("Player 1, where would you like to move?");
+                            movePosition = Convert.ToInt32(Console.ReadLine());
+                            movePosition = movePosition - 1;
+                            if (movePosition <= 0 | movePosition >= 8)
+                            {
+                                Console.WriteLine("Please enter a number between 1 and 9");
+                            }
+                            else
+                            {
+                                moveContinue = false;
+                            }
+                        }
+
+
+
+                        if (string.IsNullOrEmpty(boardArray[movePosition]))
+                        {
+                            boardArray[movePosition] = "X";
+                            inputContinue = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("This position is filled. Please choose another number");
+                        }
+
+                    }
+                }
+                if (playerTurn % 2 == 1)
+                {
+                    while (inputContinue == true)
+                    {
+                        Console.WriteLine("Player 2, where would you like to move?");
+                        movePosition = Convert.ToInt32(Console.ReadLine());
+
+                        if (string.IsNullOrEmpty(boardArray[movePosition]))
+                        {
+                            boardArray[movePosition] = "O";
+                            inputContinue = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("This position is filled. Please choose another number");
+                        }
+
+                    }
+                }
+
+                //for (int i = 0; i < 9; i++)
+                //    Console.WriteLine(boardArray[i]);
+                ab.Board(boardArray);
+                winner = ab.winCheck(boardArray);
+                
+                if (winner == "player1")
+                {
+                    Console.WriteLine("Player 1 is the winner");
+                    gameContinue = false;
+                }
+                else if (winner == "player2")
+                {
+                    Console.WriteLine("Player is the winner");
+                    gameContinue = false;
+                }
+            }
+
         }
 
     }
