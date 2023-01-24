f9cae22ee49a4274e1713ff48b41415bb53d507f
lucag101 <lucag202@gmail.com>
2023-01-23 18:51:47 -0700

    Finished part two of project

---
+++ Mission_3/Accept_Board.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission_3
+{
+    class Accept_Board
+    {
+        //Constructor
+        public Accept_Board()
+        {
+
+        }
+        public void Board(char[] args)
+        {
+
+            char[] spot = new char[9];
+            int count = 0;
+            foreach (char item in args)
+            {
+
+                if (item == 'O')
+                {
+                    spot[count] = 'O';
+                }
+                else if (item == 'X')
+                {
+                    spot[count] = 'X';
+                }
+                else
+                {
+                    spot[count] = ' ';
+                }
+                count++;
+            }
+
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  " + spot[0] + "  |  " + spot[1] + "  |  " + spot[2] + "  ");
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  " + spot[3] + "  |  " + spot[4] + "  |  " + spot[5] + "  "); ;
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  " + spot[6] + "  |  " + spot[7] + "  |  " + spot[8] + "  ");
+            Console.WriteLine("     |     |     ");
+        }
+
+        public string winCheck(char[] arr)
+        {
+            char[] option = new char[2] { 'X', 'O' };
+            string[] players = new string[2] { "player 1", "player 2" };
+            string winner = "nobody";
+            int value = 0;
+
+            foreach (char item in option)
+            { 
+                if ((arr[0] == item && arr[1] == item && arr[2] == item))
+                {
+                    winner = players[value];
+                }
+                else if (arr[3] == item && arr[4] == item && arr[5] == item)
+                {
+                    winner = players[value];
+                }
+                else if (arr[6] == item && arr[7] == item && arr[8] == item)
+                {
+                    winner = players[value];
+                }
+                else if (arr[0] == item && arr[3] == item && arr[6] == item)
+                {
+                    winner = players[value];
+                }
+                else if (arr[1] == item && arr[4] == item && arr[7] == item)
+                {
+                    winner = players[value];
+                }
+                else if (arr[2] == item && arr[5] == item && arr[8] == item)
+                {
+                    winner = players[value];
+                }
+                else if (arr[0] == item && arr[4] == item && arr[8] == item)
+                {
+                    winner = players[value];
+                }
+                else if (arr[2] == item && arr[4] == item && arr[6] == item)
+                {
+                    winner = players[value];
+                }
+                value++;
+            }
+            return winner;
+        }
+
+    }
+}
--- Mission_3/Program.cs
+++ Mission_3/Program.cs
@@ -4,9 +4,19 @@ namespace Mission_3
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main (string[] args)
+        { 
             Console.WriteLine("Hello World!");
+            char[] gameArray = new char[9] { 'O', 'O', 'O', 'X', 'O', 'O','O', 'X', 'O' };
+ 
+            Accept_Board ab = new Accept_Board();
+            ab.Board(gameArray);
+
+            string winner;
+            winner = ab.winCheck(gameArray); 
+            Console.WriteLine(winner);
         }
+
     }
+
 }
